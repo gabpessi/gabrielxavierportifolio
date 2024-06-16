affeb4e87ec262c122eb03f37941557ad61e1444
gabpessi <155784317+gabpessi@users.noreply.github.com>
2024-06-16 14:03:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/* document.getElementById("radio1.1").checked = true;  
+document.getElementById("radio2.1").checked = true;  
+document.getElementById("radio3.1").checked = true;  
+document.getElementById("radio4.1").checked = true;  
+document.getElementById("radio5.1").checked = true;  
+document.getElementById("radio6.1").checked = true;  
+document.getElementById("radio7.1").checked = true;  
+document.getElementById("radio8.1").checked = true;  
+document.getElementById("radio9.1").checked = true;  
+*/
+
+for (let i = 1; i < 10; i++) {
+    document.getElementById("radio" + i + ".1").checked = true;  
+}
