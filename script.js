a1ae5bde46a8436add20fb4a67407f6a16dcf978
gabpessi <155784317+gabpessi@users.noreply.github.com>
2024-07-22 16:24:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,32 @@ document.getElementById("radio8.1").checked = true;
 document.getElementById("radio9.1").checked = true;  
 */
 
-for (let i = 1; i < 10; i++) {
+for (let i = 1; i < 7; i++) {
     document.getElementById("radio" + i + ".1").checked = true;  
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const animatedElements = document.querySelectorAll(".animated-element");
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("in-view");
+          observer.unobserve(entry.target); // Para observar apenas uma vez
+        }
+      });
+    }, {
+      threshold: 0.1 // Inicia a animação quando 10% do elemento está visível
+    });
+  
+    animatedElements.forEach((element) => {
+      observer.observe(element);
+    });
+  });
+
+
+
+window.addEventListener('load', function() {
+    window.scrollTo(0, 0);
+  });
+
